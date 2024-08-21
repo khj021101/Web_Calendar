8f5ea9722c402d90b0a4ce9df7dea1b22840b87f
Joshua Kim <joshuakim115@gmail.com>
2024-08-21 15:18:51 +0900

    modify

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+const port = 8080;
+var path = require('path');
 
-app.listen(8080, function(){
-    console.log('listening on 8080')
+app.use(express.static(path.join(__dirname, '/')));
+
+app.listen(port, function(){
+    console.log('listening on ${port}')
 })
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html')
